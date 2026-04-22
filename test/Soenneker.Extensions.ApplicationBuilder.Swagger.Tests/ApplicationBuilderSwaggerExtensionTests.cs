edb4fd2fc49317883cdc3b8754d674c428622275
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:48 -0500

    TUnit conversion

--- test/Soenneker.Extensions.ApplicationBuilder.Swagger.Tests/ApplicationBuilderSwaggerExtensionTests.cs
+++ test/Soenneker.Extensions.ApplicationBuilder.Swagger.Tests/ApplicationBuilderSwaggerExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.ApplicationBuilder.Swagger.Tests;
 
 public sealed class ApplicationBuilderSwaggerExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
